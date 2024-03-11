127d862bc20dd23215a4b983b708b98777b1d942
Dospinescu-Rares <rares.dospinescu@stud.ubbcluj.ro>
2024-03-11 15:45:40 +0200

    Somehow made the tests work

--- src/test/java/service/ServiceTest.java
+++ src/test/java/service/ServiceTest.java
@@ -2,14 +2,28 @@ package service;
 
 import domain.Student;
 import org.junit.Test;
+import repository.NotaXMLRepo;
+import repository.StudentXMLRepo;
+import repository.TemaXMLRepo;
+import validation.NotaValidator;
+import validation.StudentValidator;
+import validation.TemaValidator;
 import validation.ValidationException;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import static org.junit.Assert.*;
 
 public class ServiceTest {
-
-    private Service service;
+    StudentValidator studentValidator = new StudentValidator();
+    TemaValidator temaValidator = new TemaValidator();
+    String filenameStudent = "fisiere/Studenti.xml";
+    String filenameTema = "fisiere/Teme.xml";
+    String filenameNota = "fisiere/Note.xml";
+    StudentXMLRepo studentXMLRepository = new StudentXMLRepo(filenameStudent);
+    TemaXMLRepo temaXMLRepository = new TemaXMLRepo(filenameTema);
+    NotaValidator notaValidator = new NotaValidator(studentXMLRepository, temaXMLRepository);
+    NotaXMLRepo notaXMLRepository = new NotaXMLRepo(filenameNota);
+    private Service service = new Service(studentXMLRepository, studentValidator, temaXMLRepository, temaValidator, notaXMLRepository, notaValidator);;
 
 
     @Test
@@ -30,6 +44,6 @@ public class ServiceTest {
 
         Student newStudent2 = new Student(null, "Mihai", 2, "mihai@yahoo.com");
         Throwable response2 = assertThrows(ValidationException.class, () -> service.addStudent(newStudent));
-        assertEquals("\"Id incorect!", response.getMessage());
+        assertEquals("Id incorect!", response.getMessage());
     }
 }
